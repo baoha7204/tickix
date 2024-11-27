b01cdca0385d1957a406f6a45d4c3c5766b93c64
baoha7204 <baoha3604@gmail.com>
2024-11-27 22:01:41 +0700

    [BaoHMQ][nats] feat: add base listener/publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created/publisher";
 
 console.clear();
 
@@ -6,16 +7,17 @@ const stan = nats.connect("tickix", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
+  const data = {
     id: "123",
     title: "concert",
     price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
-  });
+  };
+  try {
+    await new TicketCreatedPublisher(stan).publish(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
